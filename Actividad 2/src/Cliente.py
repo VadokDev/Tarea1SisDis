5d3b72f2c7842953b71e6d1b832ab74f0dfd6e35
Sebastian Godinez <sebastian.godinez@sansano.usm.cl>
2019-09-08 20:17:58 -0300

    2-step handshake

--- Actividad 2/src/Cliente.py
+++ Actividad 2/src/Cliente.py
@@ -6,10 +6,13 @@ DATASERVER_PORT = 5000
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect((LOCALHOST, DATASERVER_PORT))
 
-msg = ""
+# 2-step handshake
+cliente.send(bytes("Cliente", "utf-8"))
+msg = cliente.recv(1024).decode("utf-8")
+print(msg)
 # envio mensajes hasta que ingreso "Terminar"
 while msg != "Terminar":
-
+    print(">", end=" ")
     msg = input()
     cliente.send(bytes(msg, "utf-8"))
 
