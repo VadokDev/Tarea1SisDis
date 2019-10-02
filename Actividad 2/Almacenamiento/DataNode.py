dcb4467b950fb2623fdbdec516e9e7cc94a7868f
Sebastian Godinez <sebastian.godinez@sansano.usm.cl>
2019-10-02 00:34:25 -0300

    hice su carpeta y se conecta

---
+++ Actividad 2/Almacenamiento/DataNode.py
@@ -0,0 +1,30 @@
+import socket
+
+SERVERIP = "server" # definido en docker-compose
+DATASERVER_PORT = 5000
+
+datanode = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+datanode.connect((SERVERIP, DATASERVER_PORT))
+
+# 3-step handshake
+datanode.send(bytes("Hola", "utf-8")) # envio
+msg = datanode.recv(1024).decode("utf-8") # rebibo
+datanode.send(bytes("Data", "utf-8")) # envio mi tipo
+print(msg)
+
+while msg != "Terminar":
+
+    msg = datanode.recv(1024).decode("utf-8")
+
+    # recibo heartbeat
+    if msg == "hearbeat":
+        datanode.send(bytes("I'm fine", "utf-8"))
+    # en cambio, si es un mensaje:
+    elif msg[:7] == "mensaje":
+        archivo = open("data.txt", "a")
+        archivo.write(msg[8:] + "\n")
+        archivo.close()
+        datanode.send(bytes("It's fine", "utf-8"))
+
+print("Adios!")
+datanode.close()
